2c086bb00fe88980c64702a23831100a366e0709
byHYONS <byhyons@gmail.com>
2024-04-29 19:25:34 +0200

    [UPD] milestone 1 completed

--- js/main.js
+++ js/main.js
@@ -6,33 +6,40 @@ const userName = document.getElementById('nome-user');
 console.log({userName});
 const kmUser = document.getElementById('km-user');
 console.log({kmUser});
-const etaUser = document.querySelector('option[value]');
+const etaUser = document.getElementById('eta-user');
 console.log({etaUser});
 
 // applicare costanti per prezzo km e sconti:
 const prezzoKm = 0.21;
-const scontoMinorenni = 20;
-const scontoOver65 = 40;
+const prezzoMinorenne = 20;
+const prezzoOver65 = 40;
+
 
 // andare in ascolto dei campi input quando premiamo su submit:
 const submit = document.getElementById('submit');
 submit.addEventListener('click', function(e){
     e.preventDefault();
+
+    let prezzo = Number(kmUser.value) * prezzoKm;
+    console.log(prezzo);
+
+    console.log(userName.value);
+    console.log(kmUser.value);
     console.log(etaUser.value);
+
+    
+
+    if (etaUser.value === 'minorenne'){
+        const prezzoSconto =  prezzo / 100 * prezzoMinorenne;
+        console.log(prezzoSconto);
+        prezzo -= prezzoSconto;
+    
+    } else if (etaUser.value === 'over65'){
+        const prezzoSconto =  prezzo / 100 * prezzoOver65;
+        console.log(prezzoSconto);
+        prezzo -= prezzoSconto;
+    } 
+
+    // prezzo.toFixed(2);
+    console.log(`Il prezzo del bigleitto è di € ${prezzo.toFixed(2)}.`);
 });
-
-// Predisprre condizioni in base ai possibili sconti da appliccare in base all'età:
-// 
-//     IF .eta ≤ 18 
-//         prezzoTotale = (kilometri * prezzoKilometr) - prezzoMinorenni;
-//     IF ELSE eta ≥ 65
-//         prezzoTotale = (kilometri * prezzoKilometr) - prezzoOver65;
-//     ELSE
-//        prezzoTotale = (kilometri * prezzoKilometr) 
-
-
-// trasformare `prezzoTotale` con un numero che restituisca solo due cifre dopo la virgola utilizzando il metodo:
-// 
-//     prezzoTotale.toFixed(2)
-// 
-// stampo in console il risultato finale.
